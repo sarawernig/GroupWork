00d2bf003ded627e1aaba40320631136be61f5eb
Fluorine <sara.wernig-zorc@gu.se>
2017-06-14 10:47:52 +0200

    Update

--- script.py
+++ script.py
@@ -3,12 +3,20 @@ import sys
 import numpy
 import argparse
 
-fin = open (sys.argv[*], 'r')
-for line in fin:
-    lines = line.rstrip('\n')
-    if line[0] == '>':
-       name = line
-       id.append(name)
-    else:
-       sequence += line
+filename = open (sys.argv[*], 'r')
 
+numberA = filename.count(A)
+numberC = filename.count(C)
+numberG = filename.count(G)
+numberT = filename.count(T)
+total = numberA + numberC + numberG + numberT
+
+perA = numberA / total
+perC = numberC / total
+perG = numberG / total
+perT = numberT / total
+
+print(perA)
+print(perC)
+print(perG)
+print(perT)
