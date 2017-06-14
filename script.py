5d9a51ba26c47e71ef4f6437f405bf38a3a6949b
DinaGMansour <dina.mansour_aly@med.lu.se>
2017-06-14 10:44:24 +0200

    Create script.py

--- script.py
+++ script.py
@@ -4,4 +4,11 @@ import numpy
 import argparse
 
 fin = open (sys.argv[*], 'r')
+for line in fin:
+    lines = line.rstrip('\n')
+    if line[0] == '>':
+       name = line
+       id.append(name)
+    else:
+       sequence += line
 
